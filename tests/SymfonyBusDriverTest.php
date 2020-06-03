9d714d967e53a71560f6c5b1f89071a0df554332
Tobias Nyholm <tobias.nyholm@gmail.com>
2020-06-03 12:07:09 +0200

    Move exception logger to a dedicated class (#4)
    * Move exception logger to a dedicated class
    
    * minor
    
    * cs

--- tests/SymfonyBusDriverTest.php
+++ tests/SymfonyBusDriverTest.php
@@ -8,13 +8,10 @@ use Happyr\BrefMessenger\SymfonyBusDriver;
 use Happyr\BrefMessenger\Test\Fixture\DummyEventDispatcher;
 use Happyr\BrefMessenger\Test\Fixture\DummyMessage;
 use PHPUnit\Framework\TestCase;
-use Psr\Log\AbstractLogger;
 use Psr\Log\NullLogger;
 use Symfony\Component\Messenger\Envelope;
-use Symfony\Component\Messenger\Event\WorkerMessageFailedEvent;
 use Symfony\Component\Messenger\Event\WorkerMessageHandledEvent;
 use Symfony\Component\Messenger\Event\WorkerMessageReceivedEvent;
-use Symfony\Component\Messenger\Exception\HandlerFailedException;
 use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Messenger\Stamp\ConsumedByWorkerStamp;
 use Symfony\Component\Messenger\Stamp\ReceivedStamp;
@@ -48,36 +45,5 @@ class SymfonyBusDriverTest extends TestCase
         $this->assertInstanceOf(WorkerMessageHandledEvent::class, $events[1]);
         $this->assertInstanceOf(WorkerMessageReceivedEvent::class, $events[2]);
         $this->assertInstanceOf(WorkerMessageHandledEvent::class, $events[3]);
-    }
-
-    public function testLogHandlerFailedException()
-    {
-        $fooMessage = new DummyMessage('Foo');
-        $bus = $this->getMockBuilder(MessageBusInterface::class)->getMock();
-
-        $envelope = new Envelope($fooMessage, [new ReceivedStamp('transport'), new ConsumedByWorkerStamp()]);
-        $bus->expects($this->at(0))->method('dispatch')
-            ->with($envelope)
-            ->willThrowException(new HandlerFailedException($envelope, [new \RuntimeException('Foo')]));
-
-        $logger = $this->getMockBuilder(AbstractLogger::class)
-            ->disableOriginalConstructor()
-            ->onlyMethods(['log'])
-            ->getMock();
-        $logger->expects($this->once())->method('log')
-            ->with('error', $this->callback(function ($message) {
-                $this->assertEquals('Handling {class} caused an HandlerFailedException: Foo', $message);
-
-                return true;
-            }), $this->anything());
-
-        $dispatcher = new DummyEventDispatcher();
-        $worker = new SymfonyBusDriver($logger, $dispatcher);
-        $worker->putEnvelopeOnBus($bus, new Envelope($fooMessage), 'transport');
-
-        $events = $dispatcher->getEvents();
-        $this->assertCount(2, $events);
-        $this->assertInstanceOf(WorkerMessageReceivedEvent::class, $events[0]);
-        $this->assertInstanceOf(WorkerMessageFailedEvent::class, $events[1]);
     }
 }
