07d4c8189f463fec7e146500eec2c95261ccff4a
Tobias Nyholm <tobias.nyholm@gmail.com>
2023-01-04 18:24:43 +0100

    Bump CI rules (#9)
    * bump CI rules
    
    * fix

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,13 @@
+<?php
+
+$finder = (new PhpCsFixer\Finder())
+    ->in(__DIR__.'/src')
+    ->in(__DIR__.'/tests')
+;
+
+return (new PhpCsFixer\Config())
+    ->setRules([
+        '@Symfony' => true,
+    ])
+    ->setFinder($finder)
+;
